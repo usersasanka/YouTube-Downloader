8f87cf52776cb761052d688ddd8f01ee6faefb6e
Sasanka Liyanage <115139715+usersasanka@users.noreply.github.com>
2023-04-15 15:03:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ))
-    API_HASH = os.getenv("API_HASH", '')
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '')
+    API_ID = int(os.getenv("24199691", ))
+    API_HASH = os.getenv("031dcee4f708de013fff3c38a3d4fd1a", '')
+    BOT_TOKEN = os.getenv("6086320794:AAFOZQPCcx1lcd3CNxzwrUh8zjbYd0i2q1M", '')
